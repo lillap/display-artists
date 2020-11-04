a0a6042956be5a1779c24b667d5b0216f2bb2a71
NORDIC\pkarim <paria.karim@gmail.com>
2020-11-04 21:53:48 +0100

    completed displayArtist and generate randomArtist

--- js/script.js
+++ js/script.js
@@ -1,58 +1,66 @@
 
-function Artist(name, country, genre, lyricSnippet) {
+let artists = [];
+
+const artist1 = new Artist('Madonna', 'USA', 'Pop', 'resources/madonna.jpg');
+const artist2 = new Artist('Fleetwood Mac', 'UK', 'Blues/Pop/Rock','resources/fleetwood.jpg');
+const artist3 = new Artist('LÉON', 'Sweden', 'Pop','resources/leon.jpg');
+const artist4 = new Artist('Christine and the Queens', 'France', 'Electronic pop','resources/christine.jpg');
+const artist5 = new Artist('Led Zeppelin', 'UK', 'Rock', 'resources/zeppelin.jpg');
+const artist6 = new Artist('Florence + the Machine', 'UK', 'Soul/Rock','resources/florence.jpg');
+const artist7 = new Artist('ABBA', 'Sweden', 'Disco/Pop/Schlager', 'resources/abba.jpg');
+const artist8 = new Artist('Death Cab for Cutie', 'USA', 'Indie','resources/deathcab.jpg');
+
+artists.push(artist1, artist2, artist3, artist4, artist5, artist6, artist7, artist8);
+
+let artistSongLyric = new Map([
+    [artist1.name,'You´re so consumed with how much you get, You waste your time with hate and regret'],
+    [artist2.name, 'Tell me lies, tell me sweet little lies'],
+    [artist3.name, 'Maybe we´ve been chasing a feeling, maybe it can be like before'],
+    [artist4.name, 'Voglio che tu mi tocchi con la tua rabbia, voglio che tu mi tocchi con il tuo furore'],
+    [artist5.name, 'But now it´s time for me to go, the autumn moon lights my way'],
+    [artist6.name, 'That´s the last song you´ll ever sing, held him down, broke his neck'],
+    [artist7.name, 'So when you´re near me, darling can´t you hear me S. O. S. The love you gave me, nothing else can save me'],
+    [artist8.name, 'Flying in circles inside a jar, `cause the air we breathe, is thinning with the words that we speak']
+])
+
+function Artist(name, country, genre,img) {
     this.name = name;
     this.country = country;
     this.genre = genre;
-    this.lyricSnippet = lyricSnippet;
+    this.img = img;
+    this.perform = () => artistSongLyric.get(name);
 }
 
-let artists = [];
-
-function addArtist(){
-
-    const artist1 = new Artist('Madonna', 'USA', 'Pop', 'bla');
-    const artist2 = new Artist('Fleetwood Mac', 'UK', 'Blues/Pop/Rock', 'bla');
-    const artist3 = new Artist('LÉON', 'Sweden', 'Pop','bla');
-    const artist4 = new Artist('Christine and the Queens', 'France', 'Electronic pop', 'bla');
-    const artist5 = new Artist('Led Zeppelin', 'UK', 'Rock', 'bla');
-    const artist6 = new Artist('Florence + the Machine', 'UK', 'Soul/Rock', 'bla');
-
-    artists.push(artist1, artist2, artist3, artist4, artist5, artist6);
-    console.log(artists);
-}
-
-function populateRandomArtist(artists){
-    return artists[Math.floor(Math.random()*artists.length)];
-}
-
-function perform(){
-    
+function createArtistCard(artist) {
+    return '<div class="card">' + 
+        '<img src="' + artist.img + '" class="card-img-top" alt="">' +   
+        '<div class="card-body">' +
+            '<h5 class="card-title" id="artistName">' + artist.name +'</h5>' +
+            '<p id="countryOfOrigin"><strong>Country: </strong>' + artist.country + '</p>' +
+            '<p id="genre"><strong>Genre: </strong>' + artist.genre + '</p>' +
+            '<p class="card-text" id="stringLyric"><strong>Lyrics: </strong><i>' +  artist.perform() + '</i></p>' +
+        '</div>' +
+    '</div>'
 }
 
 function printArtistDeck(){
     let str = '<div class="card-deck">';
+    
     artists.forEach(function(artist) {
-
-    str += '<div class="col-md-3 mb-3">' +
-                '<div class="card">' + 
-                    '<img src="..." class="card-img-top" alt="...">' +   
-                    '<div class="card-body">' +
-                        '<h5 class="card-title" id="artistName">' + artist.name +'</h5>' +
-                        '<p id="countryOfOrigin">Country: ' + artist.country + '</p>' +
-                        '<p id="genre">Genre: ' + artist.genre + '</p>' +
-                        '<a href="#" class="btn btn-primary" id="getLyricsBtn">Get Lyric!</a>' + 
-                        '<p class="card-text" id="stringLyric"> ' + '</p>' +
-                    '</div>' +
-                '</div>'+ 
-            '</div>' 
-  }); 
+        str += '<div class="col-md-3 mb-3">' + createArtistCard(artist) + '</div>';
+    }); 
   
-  str += '</div>';
-  document.getElementById("cardContainer").innerHTML = str;
+    str += '</div>';
+    document.getElementById("cardContainer").innerHTML = str;
 }
-  addArtist();
-  printArtistDeck();
 
-const getLyricsBtn  = document.getElementById('getLyricsBtn');
+function populateRandomArtist(){
+    const randomArtist = artists[Math.floor(Math.random()*artists.length)];
+    const artistOfTheDay = createArtistCard(randomArtist);
+    document.getElementById("artistOfTheDay").innerHTML = artistOfTheDay;
 
-getLyricsBtn.addEventListener('click', perform);
+}
+printArtistDeck();
+populateRandomArtist();
+
+
